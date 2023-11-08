dc2fc97b3693fc6c9d890a888f180bf2db84465b
Álvaro Pimentel <alvaropimentel124@gmail.com>
2023-11-08 18:08:02 +0100

    Refactor landing page view to display product information

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,45 +3,48 @@
 @section('content')
 <!DOCTYPE html>
 <html>
+
 <head>
 </head>
+
 <body>
-   <div class="container">
-       <div class="row">
-           <div class="col-md-12">
-            <h1>Landing Page</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Landing Page</h1>
 
 
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>Nombre</th>
-                                    <th>Descripción</th>
-                                    <th>Imagen</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($products as $product)
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>Nombre</th>
+                                        <th>Descripción</th>
+                                        <th>Imagen</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($products as $product)
                                     <tr>
                                         <td>{{ $product->name }}</td>
                                         <td>{{ $product->description }}</td>
                                         <td><img src="{{ $product->image_url }}" alt="{{ $product->name }}" width="100"></td>
                                         <td>{{ $product->price}} €</td>
                                     </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            
-           </div>
-       </div>
+
+            </div>
+        </div>
 </body>
+
 </html>
 
 @endsection
