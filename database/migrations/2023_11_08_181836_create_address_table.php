b3f54eb942e41f8f43d1d77915b6792dcf06228e
Álvaro Pimentel <alvaropimentel124@gmail.com>
2023-11-08 19:29:01 +0100

    Refactor user table schema to use full name instead of separate name and last name fields

---
+++ database/migrations/2023_11_08_181836_create_address_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('address', function (Blueprint $table) {
+            $table->id();
+            $table->string('direccion');
+            $table->string('ciudad');
+            $table->string('poblacion');
+            $table->string('codigo_postal');
+            $table->string('numero')->nullable();
+            $table->string('piso')->nullable();
+            $table->string('puerta')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('address');
+    }
+};
