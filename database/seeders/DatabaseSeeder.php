d97aeb70ec4d76c6b140cc085aba21d65d4ecc0f
GregoriSerra <93575453+GregoriSerra@users.noreply.github.com>
2023-12-15 20:16:05 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,41 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-/*
-        \App\Models\User::factory()->create([
-        'name' => 'Test User',
-        'email' => 'test@example.com',
-
-        ]);
-        
-
-        \App\Models\Vendor::factory()->create([[
-            'Name' => 'Estimazon store',
-            'Description' => 'This is vendor 1',
-            'Address' => '123 Main St',
-            'Phone' => '555-1234',
-            'Email' => 'store@estimazon.com',
-            'NIF' => '12345678A'
-        ],
-        [
-            'Name' => 'Miravia store',
-            'Description' => 'This is vendor 2',
-            'Address' => '456 Elm St',
-            'Phone' => '555-5678',
-            'Email' => 'store@miravia.com',
-            'NIF' => '87654321B'
-        ],
-        [
-            'Name' => 'Paquita store',
-            'Description' => 'This is vendor 3',
-            'Address' => '789 Oak St',
-            'Phone' => '555-9012',
-            'Email' => 'store@paquita.com',
-            'NIF' => '11111111C'
-        ]]);
-        */
-
 
         //call product seeder and vendor seeder
         $sql = File::get(database_path('seeds/full_provinces.sql'));
