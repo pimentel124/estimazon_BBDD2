cd72263382cfd5ed74bc1c1e4fa4365e0fe5ec8b
GregoriSerra <93575453+GregoriSerra@users.noreply.github.com>
2023-12-15 20:07:35 +0100

    Update Province.php

--- app/Models/Province.php
+++ app/Models/Province.php
@@ -6,10 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Province extends Model
 {
-    protected $table = 'provinces'; // Nombre correcto de la tabla
+    protected $table = 'provinces'; 
     protected $fillable = ['id', 'idCCAA', 'name'];
 
-    // En el modelo Province
 public function addresses()
 {
     return $this->hasMany(Address::class, 'province_id');
